b3e6680481cabab236e02497a3630a25a1a8879f
adamcheasley <adam.cheasley@gmail.com>
2009-12-05 19:18:23 +0000

    basic test file

---
+++ tests.py
@@ -0,0 +1,15 @@
+import unittest
+from pyrc import IRC
+
+
+class Tests(unittest.TestCase):
+    """
+    Contains all the tests.
+    """
+
+    def test_Test(self):
+        self.assertEqual(1, 1)
+
+
+if __name__ == '__main__':
+        unittest.main()
